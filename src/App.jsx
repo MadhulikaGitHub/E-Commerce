34a7f8acb0cb62f643411198f009945314bfb4a3
MadhulikaGitHub <er.madhulikasharma@gmail.com>
2024-07-11 12:05:22 +0530

    11 July 2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Routes, Route } from "react-router-dom"
+import axios from 'axios'
 import Header from './component/layout/Header'
 import Home from './component/Home'
 import Footer from './component/layout/Footer'
@@ -19,15 +20,27 @@ import Shipping from './component/cart/Shipping'
 import CheckoutStep from './component/cart/CheckoutStep'
 import ConfirmOrder from './component/cart/ConfirmOrder'
 import Payment from './component/payment/Payment'
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 function App() {
+
+  const [stripeApiKey, setStripeApiKey] = useState("");
+
+  async function getStripeApiKey() {
+    const { data } = await axios.get("/api/stripeapiKey");
+    setStripeApiKey(data.stripeApiKey);
+  }
+
+  //console.log(stripeApiKey)
 
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(loadUser())
+    getStripeApiKey()
   }, [dispatch])
-  
+
   return (
     <>
       <Header />
@@ -41,10 +54,23 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/checkout" element={<Checkout />} />
-        <Route path="/checkoutStep" element={<CheckoutStep/>} />
-        <Route path="/shipping" element={<Shipping/>} />
-        <Route path="/order/confirm" element={<ConfirmOrder/>} />
-        <Route path="/payment" element={<Payment/>} />
+        <Route path="/checkoutStep" element={<CheckoutStep />} />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/order/confirm" element={<ConfirmOrder />} />
+
+        {
+          stripeApiKey && (
+            <Route
+              path="/payment"
+              element={
+                <Elements stripe={loadStripe(stripeApiKey)}>
+                  <Payment />
+                </Elements>
+              }
+            />
+          )
+        }
+        
 
       </Routes>
       <Footer />
